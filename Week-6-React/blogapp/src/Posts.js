1e22bb51dae3800ade891d3cffd8e15695c57291
Ujjwal-coder2003 <ujjwalsau17@gmail.com>
2025-07-27 19:30:17 +0530

    done

---
+++ Week-6-React/blogapp/src/Posts.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(data => {
+        const postList = data.map(p => new Post(p.id, p.title, p.body));
+        this.setState({ posts: postList });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    alert("Something went wrong: " + error);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Blog Posts</h2>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+export default Posts;
---
+++ Week-6-React/myfirstreact/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the first session of React</h1>
+      <h1>This is my first react program</h1>
+    </div>
+  );
+}
+
+export default App;
